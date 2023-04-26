a54bc81e43c7edb22febe16a9a2d1e6830d56c77
Erik Hofer <erik-hofer@mail.de>
2023-04-26 19:51:36 +0200

    Implement database result

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,71 @@
-import { Accordion } from 'solid-bootstrap'
-import { Component, createSignal } from 'solid-js'
-import Highlight from "solid-highlight"
-import "highlight.js/styles/default.css"
-// @ts-ignore
-import lexer from "sql-parser/lib/lexer"
-import { tokenize } from './InjectionVisualizer/lexer'
-import { InjectionVisualizer, VulenerableSql } from './InjectionVisualizer/InjectionVisualizer'
+import { Accordion, Alert } from "solid-bootstrap";
+import { Component, createEffect, createSignal, ErrorBoundary } from "solid-js";
+import Highlight from "solid-highlight";
+import "highlight.js/styles/default.css";
+import {
+  InjectionVisualizer,
+  VulenerableSql,
+} from "./InjectionVisualizer/InjectionVisualizer";
+import { DatabaseResult } from "./DatabaseResult/DatabaseResult";
 
 const code = `username = getRequestString("username")
 password = getRequestString("password")
 
 sql = "SELECT * FROM users WHERE name = '" + username + "' AND password = '" + password + "'"
-`
-
-//const tokens = lexer.tokenize('select * from my_table')
-//console.log(tokens)
-
+`;
 
 const App: Component = () => {
-  const [username, setUsername] = createSignal('erik')
-  const [password, setPassword] = createSignal('123')
+  const [username, setUsername] = createSignal("erik");
+  const [password, setPassword] = createSignal("123");
 
   const sql: () => VulenerableSql = () => [
-    { type: 'static', text: "SELECT * FROM users WHERE name = '" },
-    { type: 'dynamic', text: username()  },
-    { type: 'static', text: "' AND password = '" },
-    { type: 'dynamic', text: password() },
-    { type: 'static', text: "'" }
-  ]
+    { type: "static", text: "SELECT * FROM users WHERE name = '" },
+    { type: "dynamic", text: username() },
+    { type: "static", text: "' AND password = '" },
+    { type: "dynamic", text: password() },
+    { type: "static", text: "'" },
+  ];
+
+  const sqlString = () =>
+    sql()
+      .map((p) => p.text)
+      .join("");
 
   return (
-    <div style={{ display: "flex", "flex-direction": "column", "align-items": "center" }}>
-      <div style={{ width: "300px" }} class="mt-5">
+    <div
+      style={{
+        display: "flex",
+        "flex-direction": "column",
+        "align-items": "center",
+      }}
+      class="mt-5 mb-5"
+    >
+      <div style={{ width: "300px" }}>
         <form>
           <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
           <input type="text" style="display:none" />
           <input type="password" style="display:none" />
 
           <div class="form-floating mb-1">
-            <input type="text" class="form-control" id="floatingInput" value={username()} onInput={(e) => setUsername(e.currentTarget.value)} autocomplete="nope" />
+            <input
+              type="text"
+              class="form-control"
+              id="floatingInput"
+              value={username()}
+              onInput={(e) => setUsername(e.currentTarget.value)}
+              autocomplete="nope"
+            />
             <label for="floatingInput">Username</label>
           </div>
           <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" value={password()} onInput={(e) => setPassword(e.currentTarget.value)} autocomplete="new-password" />
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              value={password()}
+              onInput={(e) => setPassword(e.currentTarget.value)}
+              autocomplete="new-password"
+            />
             <label for="floatingPassword">Password</label>
           </div>
         </form>
@@ -62,6 +85,14 @@ const App: Component = () => {
           <Accordion.Header>SQL</Accordion.Header>
           <Accordion.Body>
             <InjectionVisualizer sql={sql()} />
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <Accordion style={{ width: "1200px" }} class="mt-5">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Result</Accordion.Header>
+          <Accordion.Body>
+            <DatabaseResult query={sqlString()} />
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
---
+++ src/DatabaseResult/DatabaseResult.tsx
@@ -0,0 +1,61 @@
+import { Alert, Table } from "solid-bootstrap";
+import { For, Show } from "solid-js";
+import initSqlJs from "sql.js";
+
+const INIT_SQL = `
+CREATE TABLE users (id int, name char, password char);
+INSERT INTO users VALUES (1, 'erik', '123');
+INSERT INTO users VALUES (2, 'admin', 'admin');
+INSERT INTO users VALUES (3, 'justus', '111');
+INSERT INTO users VALUES (4, 'peter', '222');
+INSERT INTO users VALUES (5, 'bob', '333');
+`;
+
+const SQL = await initSqlJs({
+  // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+  // You can omit locateFile completely when running in node
+  locateFile: (file) => `https://sql.js.org/dist/${file}`,
+});
+
+const db = new SQL.Database();
+db.run(INIT_SQL);
+
+export function DatabaseResult(props: { query: string }) {
+  const execResult: () => { results: initSqlJs.QueryExecResult[], err?: unknown } = () => {
+    try {
+      return {results: db.exec(props.query)}
+    } catch (err) {
+      return {results: [], err}
+    }
+  }
+
+  const user = () => execResult().results[0]?.values[0][1]
+
+  return (
+    <Show when={!execResult().err} fallback={<Alert variant="danger">{"" + execResult().err}</Alert>}>
+      <Show when={user()} fallback={<Alert variant="warning">Invalid credentials</Alert>}>
+        <Alert variant="success">Logged in as <b>{"" + user()}</b>.</Alert>
+      </Show>
+      <For each={execResult().results}>
+        {(result) => (
+          <Table striped bordered>
+            <thead>
+              <tr>
+                <For each={result.columns}>{(column) => <th>{column}</th>}</For>
+              </tr>
+            </thead>
+            <tbody>
+              <For each={result.values}>
+                {(row) => (
+                  <tr>
+                    <For each={row}>{(value) => <td>{"" + value}</td>}</For>
+                  </tr>
+                )}
+              </For>
+            </tbody>
+          </Table>
+        )}
+      </For>
+    </Show>
+  );
+}
